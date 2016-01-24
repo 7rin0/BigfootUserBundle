71e3344b6ee0c88ec2b22fed1c7973801903e969
Luis SEVERINO <lfs.severino@gmail.com>
2016-01-24 01:19:12 +0100

    Misc fixies; BO fully works. Next fix js

--- Form/Type/UserType.php
+++ Form/Type/UserType.php
@@ -4,13 +4,17 @@ namespace Bigfoot\Bundle\UserBundle\Form\Type;
 
 use Bigfoot\Bundle\ContextBundle\Service\ContextService;
 use Bigfoot\Bundle\UserBundle\Event\UserEvent;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\EventDispatcher\GenericEvent;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Tests\Extension\Core\Type\PasswordTypeTest;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Security\Core\Authorization\AuthorizationChecker;
 
@@ -97,7 +101,7 @@ class UserType extends AbstractType
                 )
                 ->add(
                     'formRoles',
-                    'entity',
+                    EntityType::class,
                     array(
                         'class'    => 'BigfootUserBundle:Role',
                         'multiple' => true,
@@ -108,9 +112,9 @@ class UserType extends AbstractType
         $builder
             ->add(
                 'plainPassword',
-                'repeated',
+                RepeatedType::class,
                 array(
-                    'type'     => 'password',
+                    'type'     => PasswordType::class,
                     'required' => false,
                 )
             );
